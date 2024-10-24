f1a6e152e52e893b92b3bcdb9da89d80055d2340
abbasova-senuber03 <abbasovasenuber8@gmail.com>
2024-10-24 19:54:40 +0400

    first commit

--- src/main/java/az/edu/turing/Main.java
+++ src/main/java/az/edu/turing/Main.java
@@ -2,7 +2,10 @@ package az.edu.turing;
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.print("Hello, World");
         System.out.println("Hello, World!");
+        System.out.printf("Hello
     }
 }
+
+
