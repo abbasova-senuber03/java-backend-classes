9e111e9132d28099c469cfc5745d03fdfc90af08
abbasova-senuber03 <abbasovasenuber8@gmail.com>
2024-10-26 18:25:48 +0400

    clasroom2

--- src/main/java/az/edu/turing/Main.java
+++ src/main/java/az/edu/turing/Main.java
@@ -2,10 +2,21 @@ package az.edu.turing;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("Hello, World");
-        System.out.println("Hello, World!");
-        System.out.printf("Hello
-    }
+
+        int number1 = 15;
+        int number2 = 7;
+
+       int a=number1 + number2;
+       int b=number1 - number2;
+       int c=number1 / number2;
+       int d=number1 * number2;
+       int e=number1 % number2;
+//       int f=number1 // number2;//       }
+
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+System.out.println(d);
+System.out.println(e);
 }
-
-
+}
